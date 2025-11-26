ec31d40a35cd21b163e7ba22e04087c85c5c1f70
Cuitlahuac Ramírez <109112627+cuizok@users.noreply.github.com>
2025-11-25 22:39:19 -0600

    se agrega el endpoint para actualizar perfil.page.ts

--- Ionic/ticketsApp/src/app/pages/perfil/perfil.page.ts
+++ Ionic/ticketsApp/src/app/pages/perfil/perfil.page.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule, NavController, AlertController } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';   // <--- IMPORTANTE
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, FormsModule, HttpClientModule ],  // <--- NECESARIO PARA ngModel
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
   usuario: any = null;
 
+  contrasenaActual = "";
+  nuevaContrasena = "";
+  confirmarContrasena = "";
+
   constructor(
     private navCtrl: NavController,
-    private alertCtrl: AlertController
+    private alertCtrl: AlertController,
+    private http: HttpClient
   ) {}
 
   ngOnInit() {
@@ -26,26 +33,44 @@ export class PerfilPage implements OnInit {
     if (userData) {
       this.usuario = JSON.parse(userData);
     } else {
-      this.usuario = null;
       this.navCtrl.navigateRoot('/login');
     }
   }
 
-  async cerrarSesion() {
-    const alert = await this.alertCtrl.create({
-      header: 'Cerrar sesión',
-      message: '¿Estás seguro de que deseas cerrar sesión?',
-      buttons: [
-        { text: 'Cancelar', role: 'cancel' },
-        {
-          text: 'Sí, salir',
-          handler: () => {
-            localStorage.clear();
-            this.navCtrl.navigateRoot('/login');
-          },
-        },
-      ],
-    });
-    await alert.present();
+  guardarCambios() {
+
+    if (this.nuevaContrasena && this.nuevaContrasena !== this.confirmarContrasena) {
+      alert("Las contraseñas no coinciden");
+      return;
+    }
+
+    const body = {
+      id: this.usuario.id,
+      nombre: this.usuario.nombre,
+      correo: this.usuario.correo,
+      usuario: this.usuario.usuario,
+      contrasenaActual: this.contrasenaActual,
+      nuevaContrasena: this.nuevaContrasena,
+    };
+
+    this.http.put("http://localhost:3000/api/usuario/actualizar", body)
+      .subscribe((resp: any) => {
+        
+        // actualizar datos en localStorage
+        localStorage.setItem('user', JSON.stringify(this.usuario));
+
+        alert("Datos actualizados correctamente");
+
+        this.contrasenaActual = "";
+        this.nuevaContrasena = "";
+        this.confirmarContrasena = "";
+      }, err => {
+        alert(err.error?.error || "Error al actualizar");
+      });
+  }
+
+  cerrarSesion() {
+    localStorage.removeItem("user");
+    this.navCtrl.navigateRoot("/login");
   }
 }
